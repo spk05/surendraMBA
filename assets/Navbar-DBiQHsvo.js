f2ce1f616f91e306e733b187596184d3533ca0f6
spk05 <spk05@users.noreply.github.com>
2026-04-05 21:25:13 +0000

    deploy: 1a6f8a31c1f811f14004a585aebd864df57b8ced

--- assets/Navbar-DBiQHsvo.js
+++ assets/Navbar-DBiQHsvo.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-YrzvHxMd.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-M699RdOR.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
